ef153aa5f1fd95365f102945f5b4625cc81b9444
Nive-code <82323555+Nive-code@users.noreply.github.com>
2021-04-13 13:07:35 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,4 @@
+# This is a sample Python program Task1
+from math import pi
+radius = float(input ("Input the radius of circle: "))
+print("The area of circle with radius " + str(radius) +"is: " + str(pi*radius**2))
